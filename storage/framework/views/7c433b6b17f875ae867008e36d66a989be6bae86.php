7486187a2d75cb573870df87909ae8fb778a9237
ashutsohsonu <30366603+ashutsohsonu@users.noreply.github.com>
2022-06-19 10:43:52 +0530

    Add files via upload

---
+++ storage/framework/views/7c433b6b17f875ae867008e36d66a989be6bae86.php
@@ -0,0 +1,54 @@
+
+ 
+<?php $__env->startSection('content'); ?>
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Laravel 8 CRUD Example </h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="<?php echo e(route('posts.create')); ?>"> Create New Post</a>
+            </div>
+        </div>
+    </div>
+   
+    <?php if($message = Session::get('success')): ?>
+        <div class="alert alert-success">
+            <p><?php echo e($message); ?></p>
+        </div>
+    <?php endif; ?>
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Name</th>
+            <th>Details</th>
+            <th width="280px">Action</th>
+        </tr>
+        <?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <tr>
+            <td><?php echo e(++$i); ?></td>
+            <td><?php echo e($post->name); ?></td>
+            <td><?php echo e($post->detail); ?></td>
+            <td>
+                <form action="<?php echo e(route('posts.destroy',$post->id)); ?>" method="POST">
+   
+                    <a class="btn btn-info" href="<?php echo e(route('posts.show',$post->id)); ?>">Show</a>
+    
+                    <a class="btn btn-primary" href="<?php echo e(route('posts.edit',$post->id)); ?>">Edit</a>
+   
+                    <?php echo csrf_field(); ?>
+                    <?php echo method_field('DELETE'); ?>
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </table>
+  
+    <?php echo $posts->links(); ?>
+
+      
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('post.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\Laravel-8-CRUD-Operation\resources\views/post/index.blade.php ENDPATH**/ ?>
